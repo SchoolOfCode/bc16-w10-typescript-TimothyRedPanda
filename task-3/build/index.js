f9c4a99facf021a8e1ef1b8931907a7748927d1a
TimothyPandaRed <timothy@chewedleashphotography.com>
2024-04-13 16:42:42 +0100

    index.ts added - importing HTML elements

---
+++ task-3/build/index.js
@@ -0,0 +1,10 @@
+"use strict";
+//Import HTML elements into variables.
+const Display = document.getElementById("display");
+const JokeButton = document.getElementById("jokeBtn");
+// Fetch Joke -
+// Display the fetched joke
+// Onload - display random joke
+// if (Display) {
+//     Display.textContent = "Placeholder";
+// }
---
+++ task-3/src/index.ts
@@ -0,0 +1,13 @@
+//Import HTML elements into variables.
+const Display = document.getElementById("display");
+const JokeButton = document.getElementById("jokeBtn");
+// Fetch Joke -
+
+// Display the fetched joke
+// Onload - display random joke
+
+
+
+// if (Display) {
+//     Display.textContent = "Placeholder";
+// }
