bb7160696e3ac8f1cfd3746e8139e0dcc6eebcd5
TimothyRedPanda <timothy@chewedleashphotography.com>
2024-04-14 18:04:19 +0100

    GET request and GET request by ID added

--- task-4/build/index.js
+++ task-4/build/index.js
@@ -28,9 +28,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const console = __importStar(require("node:console"));
-const port = 3000;
+const musicals_json_1 = __importDefault(require("../data/musicals.json"));
+const port = 3001;
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}/`);
+});
+app.get("/", (req, res) => {
+    if (musicals_json_1.default === undefined) {
+        res.status(404).send("No musicals found");
+    }
+    res.send(musicals_json_1.default);
+});
+app.get("/:id", (req, res) => {
+    if (musicals_json_1.default[Number(req.params.id)] === undefined) {
+        res.status(404).send("Musical not found");
+    }
+    res.send(musicals_json_1.default[Number(req.params.id)]);
 });
--- task-4/src/index.ts
+++ task-4/src/index.ts
@@ -1,12 +1,29 @@
 import express from "express";
 import * as console from "node:console";
-import * as musicals from "../data/musicals.json"
+import musicals from "../data/musicals.json"
 
-const port : number = 3000;
+const port : number = 3001;
 const app = express()
 app.use(express.json())
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}/`);
 });
+
+app.get("/", (req, res) => {
+  if(musicals === undefined) {
+    res.status(404).send("No musicals found");
+  }
+  res.send(musicals);
+})
+
+app.get("/:id", (req: express.Request, res: express.Response) => {
+  if(musicals[Number(req.params.id)] === undefined) {
+    res.status(404).send("Musical not found");
+  }
+    res.send(musicals[Number(req.params.id)]);
+})
+
+
+
 
